e3928b6905a9a0026c0f15d8999f460aad59406a
Munishd24 <munishd1234@gmail.com>
2024-08-02 15:41:25 +0530

    changes

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-# database stored in cloud
-def load_jobs_from_db():
-    
-    with engine.connect() as conn:
-        result = conn.execute(text("Select * from jobs"))
-
-        jobs = []
-        for row in result.all():
-            jobs.append(row._mapping)
-        return jobs 
-    
+  
 #template
 @app.route("/")
 def hello_world():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs , company_name='open' )
 
-#api
-@app.route("/jobs")
+#json
+@app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 #host
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,6 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 
 engine = create_engine("mysql+mysqldb://sql12723055:3BIxTsZy15@sql12.freesqldatabase.com/sql12723055",
                        pool_recycle=3600, echo=True)
+
+
+# database stored in cloud
+def load_jobs_from_db():
+    
+ with engine.connect() as conn:
+    result = conn.execute(text("Select * from jobs"))
+
+    jobs = []
+    for row in result.all():
+        jobs.append(row._mapping)
+    return jobs
 
